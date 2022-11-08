05807de523e6c45a5a44f1375656525cbf43da78
mahdi <mahdifarzadi78@gmail.com>
2022-11-08 17:09:28 +0330

    check system is debain

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
+
+# Check system
+if [ ! -f /etc/lsb-release ];then
+    if ! grep -Eqi "ubuntu|debian" /etc/issue;then
+        echo "\033[1;31mOnly Ubuntu or Debian can run this shell.\033[0m"
+        exit 1
+    fi
+fi
 
 ss-server -s 0.0.0.0 -p 6000 -k ${PASSWORD:-test} -m aes-256-gcm -u --plugin v2ray-plugin --plugin-opts "server" &
 PIDS[0]=$!
