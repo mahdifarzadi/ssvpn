5f9aebb107bba67f4312227a351b3a1a1c6e1d45
mahdi <mahdifarzadi78@gmail.com>
2023-01-19 13:34:53 +0330

    add flags

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # vars
+INSTALL_DEP=false
+
 V2RAY_VERSION=v1.3.2
 
+JUST_SS=false
 SERVER_HOST=0.0.0.0
 SERVER_PORT=443
-SERVER_ADDRESS=$SERVER_HOST:$SERVER_PORT # can be unix socket
+SERVER_ADDRESS=$SERVER_HOST:$SERVER_PORT # can use unix socket path
 SERVER_PASSWORD=VERYVERYSTRONGPASS
 SERVER_ENC_METHOD=chacha20-ietf-poly1305
 SERVER_PLUGIN=v2ray-plugin
@@ -17,47 +20,165 @@ MANAGER_PORT=4000
 MANAGER_ADDRESS=$MANAGER_HOST:$MANAGER_PORT
 MANAGER_PASSWORD=VERYVERYSTRONGPASSFORMANAGER
 
+USE_TELEGRAM=false
 TELEGRAM_TOKEN=INVALID
 
-# TODO add flags
-
 ###
-# install dependencies
+# read flags
 ###
 
-apt update
+function help {
+    cat <<EOF
+help
+EOF
+    exit 0
+}
 
-# install shadowsocks-libev from repository
-apt install shadowsocks-libev -y
+# -i,--install              INSTALL_DEP=true
+# -s, --ss-server           JUST_SS=true
+# -h, --server-host         SERVER_HOST=$2
+# -p, --server-port         SERVER_PORT=$2
+# -a, --server-address      SERVER_ADDRESS=$2
+# -k, --server-password     SERVER_PASSWORD=$2
+# -m, --encrypt-method      SERVER_ENC_METHOD=$2
+# -H, --manager-host        MANAGER_HOST=$2
+# -P, --manager-port        MANAGER_PORT=$2
+# -A, --manager-address     MANAGER_ADDRESS=$2
+# -K, --manager-password    MANAGER_PASSWORD=$2
+# -t, --telegram            USE_TELEGRAM=true, TELEGRAM_TOKEN=$2
+# --v2ray-version           V2RAY_VERSION=$2
+# --plugin                  SERVER_PLUGIN=$2
+# --ip                      SERVER_IP=$2
+# --help                    Help
 
-# install v2ray-plugin
-wget https://github.com/shadowsocks/v2ray-plugin/releases/download/$V2RAY_VERSION/v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
-tar -xzf v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
-mv v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
-chmod +x /usr/local/bin/v2ray-plugin
-rm v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
+TEMP=$(
+    getopt -o ish:p:a:k:m:H:P:A:K:t: \
+        --long install,ss-server,server-host:,server-port:,server-address:,server-password:,encrypt-method:,manager-host:,manager-port:,manager-address:,manager-password:,telegram:,v2ray-version:,plugin:,ip:,help \
+        -n 'javawrap' -- "$@"
+)
 
-# install nodejs
-apt install nodejs -y
-apt install npm -y
+if [ $? != 0 ]; then
+    echo "Terminating..." >&2
+    exit 1
+fi
 
-# install redis
-apt install redis-server -y
+eval set -- "$TEMP"
 
-# install shadowsocks-manager
-npm i -g shadowsocks-manager --unsafe-perm
+while true; do
+    case "$1" in
+    -i | --install)
+        INSTALL_DEP=true
+        shift
+        ;;
+    -s | --ss-server)
+        JUST_SS=true
+        shift
+        ;;
+    -h | --server-host)
+        SERVER_HOST="$2"
+        shift 2
+        ;;
+    -p | --server-port)
+        SERVER_PORT="$2"
+        shift 2
+        ;;
+    -a | --server-address)
+        SERVER_ADDRESS="$2"
+        shift 2
+        ;;
+    -k | --server-password)
+        SERVER_PASSWORD="$2"
+        shift 2
+        ;;
+    -m | --encrypt-method)
+        SERVER_HOST="$2"
+        shift 2
+        ;;
+    -H | --manager-host)
+        MANAGER_HOST="$2"
+        shift 2
+        ;;
+    -P | --manager-port)
+        MANAGER_PORT="$2"
+        shift 2
+        ;;
+    -A | --manager-address)
+        MANAGER_ADDRESS="$2"
+        shift 2
+        ;;
+    -K | --manager-password)
+        MANAGER_PASSWORD="$2"
+        shift 2
+        ;;
+    -t | --telegram)
+        USE_TELEGRAM=true
+        TELEGRAM_TOKEN="$2"
+        shift 2
+        ;;
+    --v2ray-version)
+        V2RAY_VERSION="$2"
+        shift 2
+        ;;
+    --plugin)
+        SERVER_PLUGIN="$2"
+        shift 2
+        ;;
+    --ip)
+        SERVER_IP="$2"
+        shift 2
+        ;;
+    --help)
+        help
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *) break ;;
+    esac
+done
 
+echo $INSTALL_DEP
+echo $SERVER_HOST
+echo $SERVER_PLUGIN
+echo $SERVER_IP
+
+if $INSTALL_DEP; then
+    ###
+    # install dependencies
+    ###
+
+    apt update
+
+    # install shadowsocks-libev from repository
+    apt install shadowsocks-libev -y
+    # disable default service
+    systemctl stop shadowsocks-libev.service && systemctl disable shadowsocks-libev.service
+
+    # install v2ray-plugin
+    wget https://github.com/shadowsocks/v2ray-plugin/releases/download/$V2RAY_VERSION/v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
+    tar -xzf v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
+    mv v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
+    chmod +x /usr/local/bin/v2ray-plugin
+    rm v2ray-plugin-linux-amd64-$V2RAY_VERSION.tar.gz
+
+    # install nodejs
+    apt install nodejs -y
+    apt install npm -y
+
+    # install redis
+    apt install redis-server -y
+
+    # install shadowsocks-manager
+    npm i -g shadowsocks-manager --unsafe-perm
+fi
 
 ###
 # run services
 ###
-
-# disable default service
-systemctl stop shadowsocks-libev.service && systemctl disable shadowsocks-libev.service
-
-# TODO remove ?
-# create ss-server service
-cat << EOF > /etc/systemd/system/ss-server.service
+if $JUST_SS; then
+    # create ss-server service
+    cat <<EOF >/etc/systemd/system/ss-server.service
 [Unit]
 Description=Daemon to Shadowsocks Server
 Wants=network-online.target
@@ -70,10 +191,14 @@ ExecStart=/usr/bin/ss-server -s $SERVER_HOST -p $SERVER_PORT -k $SERVER_PASSWORD
 [Install]
 WantedBy=multi-user.target
 EOF
+    # enable and run service
+    systemctl daemon-reload
+    systemctl enable ss-server.service
+    systemctl restart ss-server.service
 
-# TODO use unix socket
-# create ss-manager service
-cat << EOF > /etc/systemd/system/ss-manager.service
+else
+    # create ss-manager service
+    cat <<EOF >/etc/systemd/system/ss-manager.service
 [Unit]
 Description=Daemon to Shadowsocks Manager
 Wants=network-online.target
@@ -87,8 +212,8 @@ ExecStart=/usr/bin/ss-manager -m $SERVER_ENC_METHOD -u --plugin $SERVER_PLUGIN -
 WantedBy=multi-user.target
 EOF
 
-# create ssmgr config file
-cat << EOF > ~/.ssmgr/ssmgr.yml
+    # create ssmgr config file
+    cat <<EOF >~/.ssmgr/ssmgr.yml
 type: s
 
 shadowsocks:
@@ -99,9 +224,8 @@ manager:
 db: 'ssmgr.sqlite'
 EOF
 
-
-# create ssmgr service
-cat << EOF > /etc/systemd/system/ssmgr.service
+    # create ssmgr service
+    cat <<EOF >/etc/systemd/system/ssmgr.service
 [Unit]
 Description=Daemon to ssmgr type s
 Wants=network-online.target
@@ -115,8 +239,14 @@ ExecStart=ssmgr -c /root/.ssmgr/ssmgr.yml
 WantedBy=multi-user.target
 EOF
 
-# create ssmgr-tel config file
-cat << EOF > ~/.ssmgr/ssmgr-tel.yml
+    # enable and run services
+    systemctl daemon-reload
+    systemctl enable ss-manager.service ssmgr.service
+    systemctl restart ss-manager.service ssmgr.service
+
+    if $USE_TELEGRAM; then
+        # create ssmgr-tel config file
+        cat <<EOF >~/.ssmgr/ssmgr-tel.yml
 type: m
 
 manager:
@@ -130,8 +260,8 @@ plugins:
 db: 'tel.sqlite'
 EOF
 
-# create telegram ssmgr service
-cat << EOF > /etc/systemd/system/ssmgr-tel.service
+        # create telegram ssmgr service
+        cat <<EOF >/etc/systemd/system/ssmgr-tel.service
 [Unit]
 Description=Daemon to ssmgr type m for telegram bot
 Wants=network-online.target
@@ -145,8 +275,9 @@ ExecStart=ssmgr -c /root/.ssmgr/ssmgr-tel.yml
 WantedBy=multi-user.target
 EOF
 
-
-# enable and run services
-systemctl daemon-reload
-systemctl enable ss-server.service ss-manager.service ssmgr.service ssmgr-tel.service
-systemctl restart ss-server.service ss-manager.service ssmgr.service ssmgr-tel.service
+        # enable and run services
+        systemctl daemon-reload
+        systemctl enable ssmgr-tel.service
+        systemctl restart ssmgr-tel.service
+    fi
+fi
